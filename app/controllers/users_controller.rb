1e7d6164ecd4eea96640bf12acfc86ddd050e618
Junaid Farooq <ijunaidfarooq@gmail.com>
2015-10-11 13:22:34 +0500

    Added Create a little and some changes to design

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(params[:name])
+
+		if @user.save
+			rediect_to @user , notice: "Helllo Weclome to Ribbit"
+		else
+			render 'new'
+		end
+	end
+
+
+
+private
+
+	def user_params
+		
+	end
+
 end
