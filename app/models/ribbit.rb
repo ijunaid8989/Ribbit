dcc34c3447335fbcab35587baa86b9ca950dd1c7
Junaid Farooq <ijunaidfarooq@gmail.com>
2015-10-12 11:58:58 +0500

    Added Ribbit form to User Profile

--- app/models/ribbit.rb
+++ app/models/ribbit.rb
@@ -1,6 +1,6 @@
 class Ribbit < ActiveRecord::Base
 	belongs_to :user
-	default_scope order: "created_at DESC"
+	default_scope { where(order: "created_at DESC") }
 
-	validate :content , length: { maximum: 140 }
+	validates :content , length: { maximum: 140 }
 end
