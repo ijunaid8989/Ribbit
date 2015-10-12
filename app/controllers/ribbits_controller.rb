1adda461552c9b18de7dec4c38b5c795c189cb6e
Junaid Farooq <ijunaidfarooq@gmail.com>
2015-10-12 18:31:41 +0500

    Added Ribbit creation and showed ribbits length

--- app/controllers/ribbits_controller.rb
+++ app/controllers/ribbits_controller.rb
@@ -1,2 +1,15 @@
 class RibbitsController < ApplicationController
+	def create
+		ribbit = Ribbit.new(ribit_params)
+		ribbit.user_id = current_user.id
+
+		flash[:error] = "Sorry Your ribbit is over 140 Chars!" unless ribbit.save
+		redirect_to root_url
+	end
+
+private
+
+def ribit_params
+	params.require(:ribbit).permit(:content)
+end
 end
--- app/models/ribbit.rb
+++ app/models/ribbit.rb
@@ -1,6 +1,6 @@
 class Ribbit < ActiveRecord::Base
 	belongs_to :user
-	default_scope { where(order: "created_at DESC") }
+	
 
 	validates :content , length: { maximum: 140 }
 end
