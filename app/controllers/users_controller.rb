8017beebbd57b1b451b0174b9f7b8975032f02be
Junaid Farooq <ijunaidfarooq@gmail.com>
2015-10-12 18:50:17 +0500

    Added New gem for Heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 
 		if @user.save
+			session[:user_id] = @user.id
 			flash[:success]= "Congrats! Welcome to ribbit!"
 			redirect_to @user
 		else
