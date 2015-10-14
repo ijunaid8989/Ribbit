93a11f8c11e00c5cc69cd015db2d1d77170bb4e4
Junaid Farooq <ijunaidfarooq@gmail.com>
2015-10-14 08:17:52 +0500

    Added RelationShip Resource and Defined asscociations along with 2 Functions Following and Follow.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,22 @@ class User < ActiveRecord::Base
 	before_validation :prep_email
 	before_save :create_avatar_url
 
+	has_many :follower_relationships , class_name: 'Relationship', foregin_key: 'followed_id'
+	has_many :followed_relationships , class_name: 'Relationship' , foregin_key: 'follower_id'
+	has_many :followers, through: :follower_relationships
+	has_many :followeds, through: :followed_relationships
+
 
 	validates :email , presence: true, uniqueness: true
 	validates :username , presence: true , uniqueness: true
 	validates :name , presence: true
 
+	def following user
+		self.followed.include? user
+	end
+	def follow user
+		Relationship.create follower_id: self.id, followed_id: user.id
+	end
 	private 
 
 	def prep_email
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :relationships
   resources :ribbits
   resources :sessions
   resources :users
